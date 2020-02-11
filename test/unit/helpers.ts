1fa3a7393669b04adbbb023ffb1d7bd6c2237cb4
punkkk <punkrus18@gmail.com>
2020-02-11 19:14:25 +0300

    add: mocking for classes (test helper), simple sum cached value test

--- test/unit/helpers.ts
+++ test/unit/helpers.ts
@@ -1,18 +1,17 @@
 import Constructable = jest.Constructable;
-import {MaybeMockedDeep} from "ts-jest/dist/util/testing";
+import MockedFunction = jest.MockedFunction;
 
-export const stubInstance = <T extends Constructable>(Class: T): MaybeMockedDeep<T> => {
+export type IMocked<T extends {prototype: any}> = {
+  [P in keyof T["prototype"]]: T[P] extends (...args: any) => any ? MockedFunction<T[P]> : T[P];
+};
+
+export const stubInstance = <T extends Constructable>(Class: T): IMocked<T> => {
   const mocked: any = {};
 
   // tslint:disable-next-line:forin
   for (const method of Object.getOwnPropertyNames(Class.prototype).filter((name) => name !== "constructor")) {
-    // tslint:disable-next-line:no-console
-    console.log(method);
     mocked[method] = jest.fn();
   }
 
-  // tslint:disable-next-line:no-console
-  console.log(mocked);
-
-  return mocked as MaybeMockedDeep<T>;
+  return mocked as IMocked<T>;
 };
--- test/unit/simple-sum.test.ts
+++ test/unit/simple-sum.test.ts
@@ -1,13 +1,26 @@
+import casual from "casual";
+
 import {stubInstance} from "./helpers";
-import {Cache} from "../../src/cache";
+import {Cache, ICache} from "../../src/cache";
+import {SimpleSum} from "../../src/multiplication/simple-sum";
 
 describe("SimpleSum", () => {
-  describe("with cache", () => {
-    const ololo = stubInstance(Cache);
+  describe("with cached value", () => {
+    it("should return cached value", () => {
+      const cache = stubInstance(Cache);
+      const simpleSum = new SimpleSum({cache: cache as ICache<string>});
+      const cachedSum = casual.uuid;
 
-    ololo.getMockName();
+      // todo fix
+      // @ts-ignore
+      cache.get.mockReturnValue(cachedSum);
+      const x = casual.uuid;
+      const y = casual.uuid;
 
-    // tslint:disable-next-line:no-console
-    console.log(ololo);
+      const sum = simpleSum.sum(x, y);
+
+      expect(sum).toBe(cachedSum);
+      expect(cache.get).toHaveBeenCalledWith(`${x}${y}`);
+    });
   });
 });
