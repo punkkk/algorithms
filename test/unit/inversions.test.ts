147e009add37cc018e65ffab5908fff003236698
punkkk <punkrus18@gmail.com>
2020-03-10 11:18:55 +0300

    add: test for inversion counter

---
+++ test/unit/inversions.test.ts
@@ -0,0 +1,24 @@
+import {Inversions} from "../../src/weeks/week-2/inversions-count";
+
+describe("Inversions", () => {
+  it("count for reversed sorted array", () => {
+    const inversions = new Inversions<number>();
+    const array = [6, 5, 4, 3, 2, 1];
+
+    expect(inversions.count(array, array.length)).toStrictEqual(15);
+  });
+
+  it("count for an 'random' array", () => {
+    const inversions = new Inversions<number>();
+    const array = [10, 4, 23, 45, 11, 20];
+
+    expect(inversions.count(array, array.length)).toStrictEqual(5);
+  });
+
+  it("count for reversed sorted array of doubles", () => {
+    const inversions = new Inversions<number>();
+    const array = [6.6, 5.5, 4.4, 3.3, 2.2, 1.1];
+
+    expect(inversions.count(array, array.length)).toStrictEqual(15);
+  });
+});
