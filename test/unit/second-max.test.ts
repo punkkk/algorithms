e4e0679e9d5da37f17bce3688165127beb712e5c
punkkk <punkrus18@gmail.com>
2020-06-01 20:46:32 +0300

    add: test for finding second max number

---
+++ test/unit/second-max.test.ts
@@ -0,0 +1,42 @@
+import casual from "casual";
+import {SecondMax} from "../../src/optional";
+
+const getLength = (max: number = 100) => casual.integer(1, Math.floor(max / 2)) * 2;
+const randomExcept = (x: number, max: number): number => {
+  const y = casual.integer(0, max - 1);
+
+  if (y !== x) {
+    return y;
+  }
+
+  return randomExcept(x, max);
+};
+
+describe("SecondMax", () => {
+  it("Should accept only array with even length", () => {
+    const numbers = new Array(getLength() + 1).fill("").map(casual.integer);
+
+    const secondMax = new SecondMax();
+
+    expect(() => {
+      secondMax.find(numbers);
+    }).toThrow("Wrong");
+  });
+
+  it("should find second biggest number", () => {
+    const secondMax = new SecondMax();
+    const length = getLength();
+    const numbers = new Array(length).fill("").map(() => casual.integer(1, 10e3));
+
+    const first = casual.integer(10e4, 10e5);
+    const second = casual.integer(10e3, 10e4);
+
+    const firstIndex = casual.integer(0, length - 1);
+    const secondIndex = randomExcept(firstIndex, length - 1);
+
+    numbers[firstIndex] = first;
+    numbers[secondIndex] = second;
+
+    expect(secondMax.find(numbers)).toEqual(second);
+  });
+});
