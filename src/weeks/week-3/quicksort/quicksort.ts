d1126dfb4f30c6921d927f83c1473fea2c52ac32
punkkk <punkrus18@gmail.com>
2020-06-04 18:06:07 +0300

    not first element as a pivot implementation for quicksort

--- src/weeks/week-3/quicksort/quicksort.ts
+++ src/weeks/week-3/quicksort/quicksort.ts
@@ -1,25 +1,47 @@
 export class Quicksort {
-  sort(array: number[]) {
-    return this.partition(array, 0, array.length);
+  sort(array: number[], left = 0, right = array.length - 1): number[] {
+    if (array.length === 1) {
+      return array;
+    }
+
+    const p = this.partition(array, left, right);
+
+    if (left < p - 1) {
+      this.sort(array, left, p - 1);
+    }
+
+    if (p < right) {
+      this.sort(array, p, right);
+    }
+
+    return array;
   }
+
   partition(array: number[], left: number, right: number) {
-    const pivot = array[left];
-    let i = left + 1;
+    const pivot = array[Math.floor(left + (right - left) / 2)];
 
-    for (let j = left + 1; j < right; j++) {
-      if (array[j] < pivot) {
-        array[i] += array[j];
-        array[j] = array[i] - array[j];
-        array[i] -= array[j];
+    let i = left;
+    let j = right;
 
+    while (i <= j) {
+      while (array[i] < pivot) {
         i += 1;
+      }
+
+      while (array[j] > pivot) {
+        j -= 1;
+      }
+
+      if (i <= j) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+        i++;
+        j--;
       }
     }
 
-    array[i - 1] += array[left];
-    array[left] = array[i - 1] - array[left];
-    array[i - 1] -= array[left];
-
-    return array;
+    return i;
   }
 }
