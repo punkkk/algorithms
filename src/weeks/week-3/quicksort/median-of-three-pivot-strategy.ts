1ef153335224e0f7cb8a7ac1ce97bf57b6e92cbc
punkkk <punkrus18@gmail.com>
2020-06-08 17:03:30 +0300

    fix: median-of-three-pivot-strategy.ts

--- src/weeks/week-3/quicksort/median-of-three-pivot-strategy.ts
+++ src/weeks/week-3/quicksort/median-of-three-pivot-strategy.ts
@@ -4,9 +4,9 @@ export class MedianOfThreePivotStrategy implements IPivotStrategy {
   get(array: number[]) {
     const first = array[0];
     const last = array[array.length - 1];
-    const middle = array[Math.floor(array.length / 2)];
+    const middle = array[this.getMiddleIndex(array)];
 
-    const arrayOfThree = [first, last, middle].sort();
+    const arrayOfThree = [first, last, middle].sort((a, b) => a - b);
 
     return {
       index: this.getPivotIndex(arrayOfThree, array, {first, middle}),
@@ -14,11 +14,15 @@ export class MedianOfThreePivotStrategy implements IPivotStrategy {
     };
   }
 
+  getMiddleIndex(array: number[]) {
+    return array.length % 2 === 0 ? array.length / 2 - 1 : Math.floor(array.length / 2);
+  }
+
   private getPivotIndex(arrayOfThree: number[], array: number[], {first, middle}: {first: number; middle: number}) {
     const median = arrayOfThree[1];
 
     if (median === middle) {
-      return Math.floor(array.length / 2);
+      return this.getMiddleIndex(array);
     }
 
     if (median === first) {
